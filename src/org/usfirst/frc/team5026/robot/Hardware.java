a729e69c507a779bf1612830a477cdca653b8235
Sam <samsarver@icloud.com>
2017-10-17 17:04:01 -0700

    Added hardware class

---
+++ src/org/usfirst/frc/team5026/robot/Hardware.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5026.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Hardware {
+
+	Spark motor1;
+	Spark motor2;
+	Spark motor3;
+	Spark motor4;
+	
+	public Hardware() {//construct hardwear
+		motor1 = new Spark(0);	
+		motor2 = new Spark(1);
+		motor3 = new Spark(2);
+		motor4 = new Spark(3);
+
+	}
+}
