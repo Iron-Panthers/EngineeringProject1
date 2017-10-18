1ca973de1196b7e65ea915cfa68793302a2cf3bc
Kaitlyn Rice <kiwidragon12345@gmail.com>
2017-10-17 17:32:21 -0700

    Deleted/Renamed Subsystems

--- src/org/usfirst/frc/team5026/robot/subsystems/Up.java
+++ src/org/usfirst/frc/team5026/robot/subsystems/Up.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class UpSubsystem extends Subsystem {
+public class Up extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
